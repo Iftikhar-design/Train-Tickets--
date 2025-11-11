de57316efa73277fcde5aca51cb74c2d886d3fe8
Levente Mozes <leventemozes@yahoo.com>
2025-11-11 12:49:35 +0000

    Added a new station

--- src/Main.kt
+++ src/Main.kt
@@ -3,9 +3,11 @@ fun main() {
         Station("Manchester Piccadilly", Money.of(55.00), Money.of(50.00)),
         Station("Birmingham New Street", Money.of(65.50), Money.of(35.00)),
         Station("Edinburgh Waverley",    Money.of(75.00), Money.of(82.00)),
-        Station("Leeds",                 Money.of(85.00), Money.of(46.00)),
-        Station("Bristol Temple Meads",  Money.of(95.00), Money.of(42.00)),
-    )
+        Station("Leeds",                 Money.of(8235.00), Money.of(46.00)),
+        Station("Bristol Temple Meads",  Money.of(915.00), Money.of(42.00)),
+        Station("Nottingham",           Money.of(1915.00), Money.of(2242.00)),
+
+        )
 
     val network = Network(initialStations)
     val fareCalc = SimpleFareCalculator()
